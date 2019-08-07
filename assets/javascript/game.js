322c959cbdacb31271c56e3ee198294dcf7553ed
Emily Tucker <emilytucker4@yahoo.com>
2019-08-06 20:45:27 -0400

    added to the math .random so that I woild get random numbers between 19 and 120 every time the page refreshes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,11 @@
-<script type = "text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-var targetNumber = Math.floor((Math.random() * 120) + 1);
+
+var targetNumber = function getRandomInteger(min, max) {
+
+    min = Math.ceil(18);
+    max = Math.floor(121);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+$("#number-to-add").text(targetNumber);
+
+var counter = 0;
