497234d14894538be5a5c936934c51e8fd529f81
Emily Tucker <emilytucker4@yahoo.com>
2019-08-09 18:04:40 -0400

    turned the cyrstls into buttons and added functions to reset the game when won or lost

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,14 +10,99 @@ $(document).ready(function () {
     $("#number-to-add").text(targetNumber);
 
     // this part is to get a random number between 1 and 12 for each crystal image 
-    var gems = Math.floor(Math.random() * 12) + 1;
-    
+    //var crystalsNumber = Math.floor(Math.random() * 12) + 1;
+
+    // pushing the text for wins losses and score to the page
     var wins = 0;
     var losses = 0;
-    var totalScore =0;
+    var totalScore = 0;
     $("#wins").text("Wins: " + wins);
     $("#losses").text("Losses: " + losses);
     $("#your-score").text(totalScore);
 
+    var magenta = Math.floor(Math.random() * 12) + 1;
+    console.log(magenta);
+    //$("#magenta").html("<img src='assets/images/magenta_crystal-512-100x100.png'>");
 
-});
+    var blue = Math.floor(Math.random() * 12) + 1;
+    console.log(blue);
+    //$("blue").html("<img src='assets/images/blue_crystal-512-100x100.png'>");
+
+    var green = Math.floor(Math.random() * 12) + 1;
+    console.log(green);
+    //$("#magenta").html("<img src='assets/images/green_crystal_copy-512-100x100.png'>");
+
+    var red = Math.floor(Math.random() * 12) + 1;
+    console.log(red);
+    //$("#red").html("<img src='assets/images/red_crystal-512-100x100.png'>");
+
+    function winner() {
+        alert("You've won the game!");
+        wins++;
+        reset();
+    };
+
+    function loser() {
+        alert("To bad, would you like to play again?")
+        losses++;
+        reset();
+    };
+
+    function reset() {
+        magenta = Math.floor(Math.random() * 12) + 1;
+        blue = Math.floor(Math.random() * 12) + 1;
+        green = Math.floor(Math.random() * 12) + 1;
+        red = Math.floor(Math.random() * 12) + 1;
+        targetNumber = function getRandomInteger(min, max) {
+
+            min = Math.ceil(18);
+            max = Math.floor(121);
+            return Math.floor(Math.random() * (max - min)) + min;
+        };
+        $("#number-to-add").text(targetNumber);
+        $("#your-score").text(totalScore);
+    };
+
+    $("#magenta").on("click", function () {
+        $("#your-score").text(totalScore);
+        totalScore = totalScore + magenta;
+        if (totalScore == targetNumber) {
+            winner();
+        }
+        else if (totalScore > targetNumber) {
+            loser();
+        };
+    })
+   
+    $("#blue").on("click", function () {
+        $("#your-score").text(totalScore);
+        totalScore = totalScore + blue;
+        if (totalScore == targetNumber) {
+            winner();
+        }
+        else if (totalScore > targetNumber) {
+            loser();
+        };
+    })
+    
+    $("#green").on("click", function () {
+        $("#your-score").text(totalScore);
+        totalScore = totalScore + green;
+        if (totalScore == targetNumber) {
+            winner();
+        }
+        else if (totalScore > targetNumber) {
+            loser();
+        };
+    })
+    
+    $("#red").on("click", function () {
+        totalScore = totalScore + red;
+        $("#your-score").text(totalScore);
+        if (totalScore == targetNumber) {
+            winner();
+        } else if (totalScore > targetNumber) {
+            loser();
+        };
+    })
+})
